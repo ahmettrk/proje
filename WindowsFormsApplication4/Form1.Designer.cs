ec930b1596022580ff1a387a3ddcac28ac0479d7
ahmettrk <46024791+ahmettrk@users.noreply.github.com>
2018-12-20 15:52:11 +0300

    proje

---
+++ WindowsFormsApplication4/Form1.Designer.cs
@@ -0,0 +1,351 @@
+﻿namespace WindowsFormsApplication4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.trackBar3 = new System.Windows.Forms.TrackBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.kordinat = new System.Windows.Forms.CheckBox();
+            this.Elayar = new System.Windows.Forms.RadioButton();
+            this.Yeşil = new System.Windows.Forms.RadioButton();
+            this.Kırmızı = new System.Windows.Forms.RadioButton();
+            this.Maviş = new System.Windows.Forms.RadioButton();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.objeyakala = new System.Windows.Forms.CheckBox();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(22, 308);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Başlat";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(124, 308);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(22, 267);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(320, 21);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(16, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(326, 240);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 41;
+            this.pictureBox1.TabStop = false;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.trackBar1);
+            this.groupBox4.Controls.Add(this.trackBar2);
+            this.groupBox4.Controls.Add(this.trackBar3);
+            this.groupBox4.Controls.Add(this.label3);
+            this.groupBox4.Controls.Add(this.label4);
+            this.groupBox4.Controls.Add(this.label5);
+            this.groupBox4.Location = new System.Drawing.Point(557, 267);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(255, 145);
+            this.groupBox4.TabIndex = 48;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Renk Paket";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(35, 19);
+            this.trackBar1.Maximum = 255;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(214, 45);
+            this.trackBar1.TabIndex = 28;
+            this.trackBar1.Value = 1;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(35, 56);
+            this.trackBar2.Maximum = 255;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(214, 45);
+            this.trackBar2.TabIndex = 29;
+            this.trackBar2.Value = 1;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // trackBar3
+            // 
+            this.trackBar3.Location = new System.Drawing.Point(35, 94);
+            this.trackBar3.Maximum = 255;
+            this.trackBar3.Name = "trackBar3";
+            this.trackBar3.Size = new System.Drawing.Size(214, 45);
+            this.trackBar3.TabIndex = 30;
+            this.trackBar3.Value = 1;
+            this.trackBar3.Scroll += new System.EventHandler(this.trackBar3_Scroll);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(14, 19);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(15, 13);
+            this.label3.TabIndex = 31;
+            this.label3.Text = "R";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(14, 56);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(15, 13);
+            this.label4.TabIndex = 32;
+            this.label4.Text = "G";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(15, 94);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(14, 13);
+            this.label5.TabIndex = 33;
+            this.label5.Text = "B";
+            // 
+            // kordinat
+            // 
+            this.kordinat.AutoSize = true;
+            this.kordinat.Location = new System.Drawing.Point(277, 331);
+            this.kordinat.Name = "kordinat";
+            this.kordinat.Size = new System.Drawing.Size(65, 17);
+            this.kordinat.TabIndex = 50;
+            this.kordinat.Text = "Kordinat";
+            this.kordinat.UseVisualStyleBackColor = true;
+            // 
+            // Elayar
+            // 
+            this.Elayar.AutoSize = true;
+            this.Elayar.Location = new System.Drawing.Point(466, 389);
+            this.Elayar.Name = "Elayar";
+            this.Elayar.Size = new System.Drawing.Size(82, 17);
+            this.Elayar.TabIndex = 24;
+            this.Elayar.TabStop = true;
+            this.Elayar.Text = "Manual Seç";
+            this.Elayar.UseVisualStyleBackColor = true;
+            // 
+            // Yeşil
+            // 
+            this.Yeşil.AutoSize = true;
+            this.Yeşil.Location = new System.Drawing.Point(466, 323);
+            this.Yeşil.Name = "Yeşil";
+            this.Yeşil.Size = new System.Drawing.Size(64, 17);
+            this.Yeşil.TabIndex = 8;
+            this.Yeşil.TabStop = true;
+            this.Yeşil.Text = "Yeşil (G)";
+            this.Yeşil.UseVisualStyleBackColor = true;
+            // 
+            // Kırmızı
+            // 
+            this.Kırmızı.AutoSize = true;
+            this.Kırmızı.Location = new System.Drawing.Point(466, 286);
+            this.Kırmızı.Name = "Kırmızı";
+            this.Kırmızı.Size = new System.Drawing.Size(71, 17);
+            this.Kırmızı.TabIndex = 6;
+            this.Kırmızı.TabStop = true;
+            this.Kırmızı.Text = "Kırmızı (R)";
+            this.Kırmızı.UseVisualStyleBackColor = true;
+            // 
+            // Maviş
+            // 
+            this.Maviş.AutoSize = true;
+            this.Maviş.Location = new System.Drawing.Point(466, 357);
+            this.Maviş.Name = "Maviş";
+            this.Maviş.Size = new System.Drawing.Size(64, 17);
+            this.Maviş.TabIndex = 7;
+            this.Maviş.TabStop = true;
+            this.Maviş.Text = "Mavi (B)";
+            this.Maviş.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox2.Location = new System.Drawing.Point(480, 21);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(326, 240);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 52;
+            this.pictureBox2.TabStop = false;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(357, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(105, 240);
+            this.richTextBox1.TabIndex = 53;
+            this.richTextBox1.Text = "";
+            // 
+            // objeyakala
+            // 
+            this.objeyakala.AutoSize = true;
+            this.objeyakala.Location = new System.Drawing.Point(277, 308);
+            this.objeyakala.Name = "objeyakala";
+            this.objeyakala.Size = new System.Drawing.Size(51, 17);
+            this.objeyakala.TabIndex = 54;
+            this.objeyakala.Text = "Kare ";
+            this.objeyakala.UseVisualStyleBackColor = true;
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.PortName = "COM3";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(22, 353);
+            this.comboBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(132, 21);
+            this.comboBox2.TabIndex = 55;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(22, 389);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 56;
+            this.button3.Text = "Open";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(124, 389);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 57;
+            this.button4.Text = "Kapat";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(830, 430);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.Elayar);
+            this.Controls.Add(this.objeyakala);
+            this.Controls.Add(this.Maviş);
+            this.Controls.Add(this.Yeşil);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.Kırmızı);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.kordinat);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.TrackBar trackBar3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.CheckBox kordinat;
+        private System.Windows.Forms.RadioButton Elayar;
+        private System.Windows.Forms.RadioButton Yeşil;
+        private System.Windows.Forms.RadioButton Kırmızı;
+        private System.Windows.Forms.RadioButton Maviş;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.CheckBox objeyakala;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ WindowsFormsApplication4/Form1.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using AForge;
+using AForge.Imaging;
+using AForge.Imaging.Filters;
+using System.Drawing.Imaging;
+using System.IO.Ports;
+
+namespace WindowsFormsApplication4
+{
+    public partial class Form1 : Form
+    {
+        private FilterInfoCollection VideoCaptureDevices;
+        private VideoCaptureDevice FinalVideo;
+
+        int R;
+        int G;
+        int B;
+
+        public Form1()
+        {
+            InitializeComponent();
+            {
+                comboBox2.DataSource = SerialPort.GetPortNames();
+                VideoCaptureDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+                foreach (FilterInfo VideoCaptureDevice in VideoCaptureDevices)
+                {
+                    comboBox1.Items.Add(VideoCaptureDevice.Name);
+                }
+                comboBox1.SelectedIndex = 0;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FinalVideo = new VideoCaptureDevice(VideoCaptureDevices[comboBox1.SelectedIndex].MonikerString);
+            FinalVideo.NewFrame += new NewFrameEventHandler(FinalVideo_NewFrame);
+            FinalVideo.Start();
+        }
+        void FinalVideo_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            Bitmap video = (Bitmap)eventArgs.Frame.Clone();
+            pictureBox1.Image = video;
+            Bitmap image = (Bitmap)eventArgs.Frame.Clone();
+            Bitmap image1 = (Bitmap)eventArgs.Frame.Clone();
+            pictureBox1.Image = image;
+
+            if (Kırmızı.Checked)
+            {
+                EuclideanColorFiltering filter = new EuclideanColorFiltering();
+                filter.CenterColor = new RGB(Color.FromArgb(215, 0, 0));
+                filter.Radius = 100;
+                filter.ApplyInPlace(image1);
+                nesnebul(image1);
+            }
+
+            if (Maviş.Checked)
+            {
+                EuclideanColorFiltering filter = new EuclideanColorFiltering();
+                filter.CenterColor = new RGB(Color.FromArgb(30, 144, 255));
+                filter.Radius = 100;
+                filter.ApplyInPlace(image1);
+                nesnebul(image1);
+            }
+
+            if (Yeşil.Checked)
+            {
+                EuclideanColorFiltering filter = new EuclideanColorFiltering();
+                filter.CenterColor = new RGB(Color.FromArgb(0, 215, 0));
+                filter.Radius = 100;
+                filter.ApplyInPlace(image1);
+                nesnebul(image1);
+            }
+
+            if (Elayar.Checked)
+            {
+                EuclideanColorFiltering filter = new EuclideanColorFiltering();
+                filter.CenterColor = new RGB(Color.FromArgb(R, G, B));
+                filter.Radius = 100;
+                filter.ApplyInPlace(image1);
+                nesnebul(image1);
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FinalVideo.Stop();
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            R = trackBar1.Value;
+        }
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            G = trackBar2.Value;
+        }
+
+        private void trackBar3_Scroll(object sender, EventArgs e)
+        {
+            B = trackBar3.Value;
+        }
+        private System.Drawing.Point[] ToPointsArray(List<IntPoint> points)
+        {
+            System.Drawing.Point[] array = new System.Drawing.Point[points.Count];
+
+            for (int i = 0, n = points.Count; i < n; i++)
+            {
+                array[i] = new System.Drawing.Point(points[i].X, points[i].Y);
+            }
+            return array;
+        }
+        public void nesnebul(Bitmap image)
+        {
+            BlobCounter blobCounter = new BlobCounter();
+            blobCounter.MinWidth = 10;
+            blobCounter.MinHeight = 10;
+            blobCounter.FilterBlobs = true;
+            blobCounter.ObjectsOrder = ObjectsOrder.Size;
+
+            BitmapData objectsData = image.LockBits(new Rectangle(0, 0, image.Width, image.Height), ImageLockMode.ReadOnly, image.PixelFormat);
+            Grayscale grayscaleFilter = new Grayscale(0.2125, 0.7154, 0.0721);
+            UnmanagedImage grayImage = grayscaleFilter.Apply(new UnmanagedImage(objectsData));
+            image.UnlockBits(objectsData);
+
+            blobCounter.ProcessImage(image);
+            Rectangle[] rects = blobCounter.GetObjectsRectangles();
+            Blob[] blobs = blobCounter.GetObjectsInformation();
+            try
+            {
+                pictureBox2.Image = image;
+            }
+            catch { }
+
+            if (objeyakala.Checked)
+            {
+                foreach (Rectangle recs in rects)
+                {
+                    if (rects.Length > 0)
+                    {
+                        Rectangle objectRect = rects[0];
+                        Graphics g = pictureBox1.CreateGraphics();
+                        using (Pen pen = new Pen(Color.FromArgb(160, 255, 160), 5))
+                        {
+                            g.DrawRectangle(pen, objectRect);
+                        }
+                        int objectX = objectRect.X + (objectRect.Width / 2);
+                        int objectY = objectRect.Y + (objectRect.Height / 2);
+
+                        g.Dispose();
+
+                        if (kordinat.Checked)
+                        {
+                            Invoke((MethodInvoker)delegate
+                            {
+                                richTextBox1.Text = objectRect.Location.ToString() + "\n" + richTextBox1.Text + "\n"; ;
+                            });
+
+                            if (objectX > 0 && objectX < 108 && objectY < 80)
+                            {
+                                serialPort1.Write("1");
+                            }
+                            if (objectX > 108 && objectX < 216 && objectY < 80)
+                            {
+                                serialPort1.Write("2");
+                            }
+                            if (objectX > 216 && objectX < 326 && objectY < 80)
+                            {
+                                serialPort1.Write("3");
+                            }
+                            if (objectX > 0 && objectX < 108 && objectY > 80 && objectY < 160)
+                            {
+                                serialPort1.Write("4");
+                            }
+                            if (objectX > 108 && objectX < 216 && objectY > 80 && objectY < 160)
+                            {
+                                serialPort1.Write("5");
+                            }
+                            if (objectX > 216 && objectX < 326 && objectY > 80 && objectY < 160)
+                            {
+                                serialPort1.Write("6");
+                            }
+                            if (objectX > 0 && objectX < 108 && objectY > 160 && objectY < 240)
+                            {
+                                serialPort1.Write("7");
+                            }
+                            if (objectX > 108 && objectX < 216 && objectY > 160 && objectY < 240)
+                            {
+                                serialPort1.Write("8");
+                            }
+                            if (objectX > 216 && objectX < 326 && objectY > 160 && objectY < 240)
+                            {
+                                serialPort1.Write("9");
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            serialPort1.BaudRate = 9600;
+            serialPort1.PortName = comboBox2.SelectedItem.ToString();
+            serialPort1.Open();
+            if (serialPort1.IsOpen)
+            {
+                MessageBox.Show("Port Açık");
+            }
+    }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            serialPort1.Close();
+            MessageBox.Show("Port Kapalı");
+        }
+    }
+}
+
